8a8e231195a9194206faca6ee361ede7f060e756
Marlon Pons Blanco <mapachestropicales@gmail.com>
2025-04-05 13:40:20 +0000

    Proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function excuseGenerator(){
+  //arrays to conform the excuse
+  const who = ['My turtle', 'My grandma', 'A radioactive', 'The president', 'A zombie'];
+  const action = ['ate', 'lost', 'crushed', 'broke', 'stole'];
+  const what = ['my homework', 'my phone', 'the car', 'my dreams'];
+  const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch'];
+  //variables to select words aleatory
+  let part1 = Math.floor(Math.random()*who.length);
+  let part2 = Math.floor(Math.random()*action.length);
+  let part3 = Math.floor(Math.random()*what.length);
+  let part4 = Math.floor(Math.random()*when.length);
+  return `${who[part1]} ${action[part2]} ${what[part3]} ${when[part4]}.`;
 };
+
+
+window.onload = function() {
+  document.querySelector("#perfectExcuse").innerHTML=excuseGenerator();
+  document.querySelector("#generateBtn").addEventListener("click",(e)=>{
+    document.querySelector("#perfectExcuse").innerHTML=excuseGenerator();
+  });
+};
+/*decidi hacer una funcion aparte (excuseGenerator) para poder darle la funcionalidad al boton.
+Antes lo tenia puesto como window.onload() cada vez que se activara el evento clicl, pero es volver a cargar toda la pagina y no creo que sea buena practica.
+La efectuada tampoco la considero la mas optima, pero es la que se me ocurrio :)*/
