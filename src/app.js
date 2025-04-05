77f6ce44600cfdb8afe6c50b75437dd334c392cb
Marlon Pons Blanco <mapachestropicales@gmail.com>
2025-04-05 20:01:59 +0000

    Cambio de etiqueta

--- src/app.js
+++ src/app.js
@@ -19,13 +19,23 @@ function excuseGenerator(){
   return `${who[part1]} ${action[part2]} ${what[part3]} ${when[part4]}.`;
 };
 
-
+/*
 window.onload = function() {
   document.querySelector("#perfectExcuse").innerHTML=excuseGenerator();
   document.querySelector("#generateBtn").addEventListener("click",(e)=>{
     document.querySelector("#perfectExcuse").innerHTML=excuseGenerator();
   });
-};
+};*/
 /*decidi hacer una funcion aparte (excuseGenerator) para poder darle la funcionalidad al boton.
-Antes lo tenia puesto como window.onload() cada vez que se activara el evento clicl, pero es volver a cargar toda la pagina y no creo que sea buena practica.
+Antes lo tenia puesto como window.onload() cada vez que se activara el evento click, pero es volver a cargar toda la pagina y no creo que sea buena practica.
 La efectuada tampoco la considero la mas optima, pero es la que se me ocurrio :)*/
+
+//Otra variante
+//En vez de usar una etiqueta button, usar un <a>, y cambiar la forma visual del link
+window.onload =function(){
+  document.querySelector("#perfectExcuse").innerHTML=excuseGenerator();
+  document.querySelector("#generate").addEventListener("click", () => {
+    document.querySelector("#perfectExcuse").innerHTML = excuseGenerator();
+  });
+}
+
